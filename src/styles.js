074d531705143d9f0cf19d317da94790e163e4b8
paanSinghCoder <gaurav3017@gmail.com>
2022-06-07 00:12:06 +0530

    feat: move project to es6

---
+++ src/styles.js
@@ -0,0 +1,83 @@
+export const containerStyles = {
+	position: 'fixed',
+	overflow: 'scroll',
+	width: '300px',
+	height: '450px',
+	display: 'none',
+	bottom: '85px',
+	zIndex: 100000,
+	wordWrap: 'break-word',
+	overflowWrap: 'break-word',
+	right: '20px',
+	marginLeft: '15px',
+	backgroundColor: '#fff',
+	borderRadius: '8px',
+	boxShadow: '2px 4px 5px 3px rgba(224, 224, 224, 0.8)'
+}
+
+export const navbarStyles = {
+	position: 'sticky',
+	top: 0,
+	backgroundColor: '#fff'
+}
+
+export const floatButtonStyles = {
+	opacity: 0.8,
+	position: 'fixed',
+	width: '70px',
+	border: 'none',
+	height: '35px',
+	bottom: '40px',
+	zIndex: 100000,
+	right: '15px',
+	backgroundColor: '#d4ebf2',
+	color: '#fff',
+	borderRadius: '50px',
+	textAlign: 'center',
+	cursor: 'pointer'
+}
+
+export const buttonDefaultOpacity = {
+	opacity: 1
+}
+
+export const listItemStyles = {
+	fontFamily: 'Arial',
+	fontSize: '13px',
+	paddingTop: '5px',
+	paddingBottom: '5px',
+	paddingLeft: '15px',
+	paddingRight: '15px'
+}
+
+export const clearBtnStyles = {
+	cursor: 'pointer',
+	float: 'right',
+	paddingRight: '10px',
+	paddingTop: '3px',
+	opacity: 0.5,
+	fontSize: '20px',
+	backgroundColor: 'transparent',
+	border: 'none'
+}
+
+export const logoTextStyles = {
+	fontFamily: 'Arial',
+	fontSize: '16px',
+	fontWeight: 'bold',
+	paddingLeft: '15px',
+	paddingTop: '3px',
+	fontStyle: 'italic',
+	color: '#525EF9'
+}
+
+export const notificationBadgeStyles = {
+	position: 'absolute',
+	display: 'none',
+	top: '-3px',
+	right: '6px',
+	width: '12px',
+	height: '12px',
+	borderRadius: '50%',
+	backgroundColor: 'red'
+}
